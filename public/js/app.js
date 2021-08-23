fb047d1aa35115006e02fe169710e00489a5111d
Ngu Alpha Tangri <ngualph12@gmail.com>
2021-08-23 15:09:29 +0100

    additional units

--- public/js/app.js
+++ public/js/app.js
@@ -14,18 +14,18 @@ fetch('/weather?address='+encodeURIComponent(location)).then((response)=>{
             console.log(data.error)
             err.textContent=data.error
         }else{
-         res.innerHTML= '<table><tr><td id="des">Temperature:</td><td> '+data.Data.weather.temperature+' degrees celcius</td></tr>'
-                        +'<tr><td id="des">Weather description: </td><td>'+data.Data.weather.weather_descriptions[0]+'</td></tr>'
-                        + '<tr><td id="des">Percentage chance of rain:</td><td> '+data.Data.weather.precip+'</td></tr>'
-                        + '<tr><td id="des">Wind_Speed:</td><td> '+data.Data.weather.wind_speed+' km/h</td></tr>'
-                        + '<tr><td id="des">Cloud Cover:</td><td> '+data.Data.weather.cloudcover+'</td></tr>'
-                        + '<tr><td id="des">Humidity:</td><td> '+data.Data.weather.humidity+'</td></tr>'
-                        + '<tr><td id="des">Pressure:</td><td> '+data.Data.weather.pressure+'Pa</td></tr>'
-                        + '<tr><td id="des">Apparent Temperature:</td><td> '+data.Data.weather.feelslike+' degrees celcius</td></tr>'
-                        + '<tr><td id="des">Wind direction:</td><td> '+data.Data.weather.wind_dir+'</td></tr>'//new row
-                        + '<tr><td id="des">Wind degree:</td><td> '+data.Data.weather.wind_degree+'</td></tr>'//new row
-                        + '<tr><td id="des">Visibility:</td><td> '+data.Data.weather.visibility+' km</td></tr>'//new row
-                        + '<tr><td id="des">Local time:</td><td> '+data.Data.loctime+'</td></tr></table>'//new row
+         res.innerHTML= '<table><tr><td id="des">Temperature:</td><td class="dta"> '+data.Data.weather.temperature+' degrees celcius</td></tr>'
+                        +'<tr><td id="des">Weather description: </td><td class="dta">'+data.Data.weather.weather_descriptions[0]+'</td></tr>'
+                        + '<tr><td id="des">Precipitation:</td><td class="dta"> '+data.Data.weather.precip+' milimeters</td></tr>'
+                        + '<tr><td id="des">Wind_Speed:</td><td class="dta"> '+data.Data.weather.wind_speed+' km/h</td></tr>'
+                        + '<tr><td id="des">Cloud Cover:</td><td class="dta"> '+data.Data.weather.cloudcover+' percent</td></tr>'
+                        + '<tr><td id="des">Humidity:</td><td class="dta"> '+data.Data.weather.humidity+' percent</td></tr>'
+                        + '<tr><td id="des">Pressure:</td><td class="dta"> '+data.Data.weather.pressure+' millibars</td></tr>'
+                        + '<tr><td id="des">Apparent Temperature:</td><td class="dta"> '+data.Data.weather.feelslike+' degrees celcius</td></tr>'
+                        + '<tr><td id="des">Wind direction:</td><td class="dta"> '+data.Data.weather.wind_dir+'</td></tr>'//new row
+                        + '<tr><td id="des">Wind degree:</td><td class="dta"> '+data.Data.weather.wind_degree+'</td></tr>'//new row
+                        + '<tr><td id="des">Visibility:</td><td class="dta"> '+data.Data.weather.visibility+' km</td></tr>'//new row
+                        + '<tr><td id="des">Local time:</td><td class="dta">'+data.Data.loctime+'</td></tr></table>'//new row
                         + '<img id="wico" src='+data.Data.weather.weather_icons[0]+' alt="weather_icon">'
          res1.innerHTML= data.location 
 
